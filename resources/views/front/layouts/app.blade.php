67cd5d67c3d8007a92cd6815d610156574945485
NohaElMandoh <noha_m_elmandoh@outlook.com>
2022-12-23 23:01:38 +0200

    update front

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -49,7 +49,7 @@
                 <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
                     <a href="#" class="navbar-brand">MENU</a>
                     <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                        <div class="navbar-nav mr-auto">
+                        <div class="navbar-nav mr-auto" style="font-size:20px">
                         <a href="{{route('admin')}}" class="nav-item nav-link ">مدير الموقع  </a>
                         <a href="#" class="nav-item nav-link ">   </a>
                         <a href="#" class="nav-item nav-link ">   </a>
@@ -136,45 +136,47 @@
                     <h2>مواقعنا</h2>
                 </div>
                 <div class="row">
-                    <div class="col-lg-3 col-md-3" style='max-width:20%'>
+                  
+                  
+                    
+                   
+                    <div class="col-lg-3 col-md-3"  style='text-align:center ;max-width:20%'>
                         <div class="service-item">
-                            <!-- <i class="flaticon-car-wash-1"></i> -->
-                        {{-- <img style='width:25%' src="{{asset('custom/front/img/location.png')}}"> --}}
-                            <i class="fa fa-map-marker-alt" style="color: red;"> </i>
-                            <h3>فرع السلام</h3>
+                        <a href=''>    <i class="fa fa-map-marker-alt" style="color: red;"></i></a>
+                        <h3>5</h3>  <h3> فرع صديان</h3>
                            
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-3"  style='max-width:20%'>
-                        <div class="service-item">
-                        <i class="fa fa-map-marker-alt" style="color: red;"></i>
-                            <h3>فرع صبابة</h3>
-                          
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3"  style='max-width:20%'>
-                        <div class="service-item">
-                        <i class="fa fa-map-marker-alt" style="color: red;"></i>
-                            <h3>فرع الشفا</h3>
-                          
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3"  style='max-width:20%'>
+                    <div class="col-lg-3 col-md-3"  style='text-align:center ;max-width:20%'>
                         <div class="service-item" >
-                        <i class="fa fa-map-marker-alt" style="color: red;"></i>
-                            <h3>فرع النقرة</h3>
+                        <a href=''>  <i class="fa fa-map-marker-alt" style="color: red;"></i></a>
+                        <h3>4</h3>    <h3>فرع النقرة</h3>
                             
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-3"  style='max-width:20%'>
+                    <div class="col-lg-3 col-md-3" style='text-align:center ;max-width:20%'>
                         <div class="service-item">
-                        <i class="fa fa-map-marker-alt" style="color: red;"></i>
-                            <h3> فرع صديان</h3>
+                        <a href='https://maps.app.goo.gl/pf9wzTiGYGrCwe4J9?g_st=ic'>  <i class="fa fa-map-marker-alt" style="color: red;"></i></a>
+                        <h3>3</h3>   <h3>فرع الشفا</h3>
+                          
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3"  style='text-align:center ;max-width:20%'>
+                        <div class="service-item">
+                        <a href='https://maps.app.goo.gl/vTFB4tPJAzDHdiPS6?g_st=ic'>   <i class="fa fa-map-marker-alt" style="color: red;"></i></a>
+                        <h3>2</h3>  <h3>فرع صبابة</h3>
+                          
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3" style='text-align:center ;max-width:20%'>
+                        <div class="service-item">
+                            <!-- <i class="flaticon-car-wash-1"></i> -->
+                        {{-- <img style='width:25%' src="{{asset('custom/front/img/location.png')}}"> --}}
+                          <a href='https://maps.app.goo.gl/UDT1rcdWM4RCakNu7?g_st=ic'>  <i class="fa fa-map-marker-alt" style="color: red;"> </i></a>
+                          <h3>1</h3>   <h3>فرع السلام</h3>
                            
                         </div>
                     </div>
-            
-                  
                   
                 </div>
             </div>
@@ -244,39 +246,40 @@
                  
                     <h2>مواقع التواصل الاجتماعى</h2>
                 </div>
-                <div class="row">
-                <div class="col-lg-3 col-md-3" style='max-width:5%'>
+                <div class="row" style="justify-content: space-around">
+                
+                    <div  >
                         <div class="service-item">
                             <!-- <i class="flaticon-car-wash-1"></i> -->
-                          
+                          <a href='https://t.me/njm_alrghwah'>  <i class="fab fa-telegram fa-3x" ></i></a>
                         
                            
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-3" >
+                    <div>
                         <div class="service-item">
-                            <!-- <i class="flaticon-car-wash-1"></i> -->
-                            <i class="fab fa-telegram" style='max-width:15px'></i>
                         
-                           
+                     <a href='https://www.tiktok.com/@njmalrgwh?_t=8YQTWtO0RIP&_r=1'>  
+                       
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License 
+                        - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                        <path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"/></svg>
+Tiktok
+                        <!-- 
+                     <i class="fab  fa-tiktok fa-3x"></i></a> -->
+                     
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-3"  style='max-width:5%'>
+                    <div >
                         <div class="service-item">
-                        
-                        <i class="fab  fa-linkedin-in"></i>
+                      <a href='https://wa.me/+966558755981'>  <i class="fab fa-whatsapp fa-3x"></i></a>
                           
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-3"  style='max-width:5%'>
-                        <div class="service-item">
-                        <i class="fab fa-whatsapp"></i>
-                          
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3"  style='max-width:5%'>
+                    <div >
                         <div class="service-item" >
-                        <i class="fab fa-snapchat"></i>
+                       <a href='https://www.snapchat.com/add/nim9999999'><i class="fab fa-snapchat fa-3x"></i></a>
                             
                         </div>
                     </div>
@@ -296,58 +299,10 @@
 
 
         <!-- Footer Start -->
-        <div class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="footer-contact">
-                            <h2>Get In Touch</h2>
-                            <p><i class="fa fa-map-marker-alt"></i>123 Street, New York, USA</p>
-                            <p><i class="fa fa-phone-alt"></i>+012 345 67890</p>
-                            <p><i class="fa fa-envelope"></i>info@example.com</p>
-                            <div class="footer-social">
-                                <a class="btn" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn" href=""><i class="fab fa-youtube"></i></a>
-                                <a class="btn" href=""><i class="fab fa-instagram"></i></a>
-                                <a class="btn" href=""><i class="fab fa-linkedin-in"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="footer-link">
-                            <h2>Popular Links</h2>
-                            <a href="">About Us</a>
-                            <a href="">Contact Us</a>
-                            <a href="">Our Service</a>
-                            <a href="">Service Points</a>
-                            <a href="">Pricing Plan</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="footer-link">
-                            <h2>Useful Links</h2>
-                            <a href="">Terms of use</a>
-                            <a href="">Privacy policy</a>
-                            <a href="">Cookies</a>
-                            <a href="">Help</a>
-                            <a href="">FQAs</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="footer-newsletter">
-                            <h2>Newsletter</h2>
-                            <form>
-                                <input class="form-control" placeholder="Full Name">
-                                <input class="form-control" placeholder="Email">
-                                <button class="btn btn-custom">Submit</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="footer" style="padding-top:0%">
+           
             <div class="container copyright">
-                <p>&copy; <a href="#">Your Site Name</a>, All Right Reserved. Designed By <a href="https://htmlcodex.com">HTML Codex</a></p>
+                <p>&copy; 2022 <a href="#">Najem al-raghwa</a>,جميع الحقوق محفوظه  <a href=""></a></p>
             </div>
         </div>
         <!-- Footer End -->
